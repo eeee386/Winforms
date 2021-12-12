33d8329f2a456096496f4563dfff320db8f5e6c4
Attila Sedon <attilasedon@pop-os.localdomain>
2021-12-12 01:05:51 +0100

    gix my commit I guess

--- MusicListASP/MusicListASP/Context/music.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE Music (
-  ID integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  Title text NOT NULL,
-  Artist text NOT NULL,
-  PublishYear integer NOT NULL,
-  MusicLength integer NOT NULL,
-  Priority integer NOT NULL,
-  UNIQUE(Title)
-);
